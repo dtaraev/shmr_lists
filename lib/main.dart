b3e3a551b5b65bc511a5636c618e9b142cee6ccc
Dmitry Taraev <dtaraev@yandex-team.ru>
2021-08-03 15:39:54 +0300

    Dynamic ListView with NotificationListener

--- lib/main.dart
+++ lib/main.dart
@@ -19,16 +19,24 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
   List<String> _items = <String>[];
+  void _addItem(String item) {
+    setState(() {
+      _items.add(item);
+    });
+  }
+
+  void _removeItem(String item) {
+    setState(() {
+      _items.remove(item);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,11 +44,41 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: AnimatedList(
-        key: _listKey,
-        itemBuilder: (context, index, animation) =>
-            _slideItem(context, animation, _items[index]),
-        initialItemCount: _items.length,
+      body: NotificationListener<ScrollNotification>(
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(_items[index]),
+              subtitle: index % 2 == 0 ? Text('subtitle') : null,
+              leading: Icon(Icons.radio_button_on),
+              onTap: () => showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: Text('Do you really want to remove this item?'),
+                  content: Text(_items[index]),
+                  actions: [
+                    TextButton(
+                      child: Text('OK'),
+                      onPressed: () {
+                        _removeItem(_items[index]);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    TextButton(
+                      child: Text('Cancel'),
+                      onPressed: () => Navigator.of(context).pop(),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: _items.length,
+        ),
+        onNotification: (scrollNotification) {
+          print('[scrollNotification] ${scrollNotification.metrics.pixels}');
+          return true;
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _addItem('Item ${_items.length}'),
@@ -48,60 +86,5 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Icon(Icons.add),
       ),
     );
-  }
-
-  Widget _slideItem(BuildContext context, Animation animation, String item) {
-    final index = _items.indexOf(item);
-    return SlideTransition(
-      position: animation.drive(Tween(
-        begin: Offset(-1, 0),
-        end: Offset(0, 0),
-      )),
-      child: ListTile(
-        title: Text(item),
-        subtitle: index % 2 == 0 ? Text('subtitle') : null,
-        leading: Icon(Icons.radio_button_on),
-        onTap: () => _promtRemoveItem(item),
-      ),
-    );
-  }
-
-  void _promtRemoveItem(String item) {
-    showDialog(
-      context: context,
-      builder: (_) => AlertDialog(
-        title: Text('Do you really want to remove this item?'),
-        content: Text(item),
-        actions: [
-          TextButton(
-            child: Text('OK'),
-            onPressed: () {
-              _removeItem(item);
-              Navigator.of(context).pop();
-            },
-          ),
-          TextButton(
-            child: Text('Cancel'),
-            onPressed: () => Navigator.of(context).pop(),
-          )
-        ],
-      ),
-    );
-  }
-
-  void _addItem(String item) {
-    _listKey.currentState?.insertItem(_items.length);
-    setState(() {
-      _items.add(item);
-    });
-  }
-
-  void _removeItem(String item) {
-    final index = _items.indexOf(item);
-    _listKey.currentState?.removeItem(
-        index, (context, animation) => _slideItem(context, animation, item));
-    setState(() {
-      _items.remove(item);
-    });
   }
 }
